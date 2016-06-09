d5cae048ca8bb792b8b93ef09cc55f1ac57063ab
EMG114 <egutierrez411@gmail.com>
2016-06-08 21:37:01 -0400

    much shorter

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -11,39 +11,46 @@
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
     
-    return nil;
+    return [self.database[season] allKeys];
+    
 }
+
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
     
-    return nil;
+    NSArray *suppliesInHoliday = self.database[season][holiday];
+    NSMutableArray *suppliesInHolidayMutable = [suppliesInHoliday mutableCopy];
+    
+    return suppliesInHolidayMutable;
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
     
-    return nil;
+    
+   return [[self.database[season] allKeys] containsObject:holiday];
+    
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
     
-    return nil;
+    return [self.database[season][holiday] containsObject:supply];
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
     
-    // no return
+     self.database[season][holiday] = [@[]mutableCopy];
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
     
-    // no return
+    [self.database[season][holiday] addObject:supply];
 }
 
 @end
